23a6f599d18d417f5d854bf273c89f5c0f85132f
Ben <ben@doginthehat.com.au>
2013-03-01 10:43:34 +1100

    Module definition class

---
+++ classes/dogtools_module.php
@@ -0,0 +1,15 @@
+<?
+
+class DogTools_Module extends Core_ModuleBase {
+
+	protected function createModuleInfo() {
+
+		return new Core_ModuleInfo(
+			"Dogtools",
+			"Small utilities for the Lemonstand platform.",
+			"Dog in the hat"
+		);
+	}
+	
+
+}
